bec0f8e683d35bff4e70dc4a2d009ce823faf1b9
Techwizz <paulrblack.prb@gmail.com>
2022-09-03 01:55:30 +0000

    Update 'index.js'

--- index.js
+++ index.js
@@ -42,7 +42,10 @@ bot.on('messageCreate', (message) => {
         message.channel.send('Red Luigi');
     }
     else if(message.content.toLowerCase().includes('how neat is that')){
-        message.channel.send('that is pretty neat!');
+        message.channel.send("that's pretty neat!");
+    }
+    else if(message.content.toLowerCase().includes("that's pretty neat")){
+        message.channel.send('how neat is that?');
     }
     else if(message.content.toLowerCase().includes('tit') || message.content.toLowerCase().includes('cock') || message.content.toLowerCase().includes('ass')){
        if (message.content.toLowerCase().includes('title') || message.content.toLowerCase().includes('class')){ //These are not bad words and will be bypassed
